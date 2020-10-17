6350d490ab18d2649705fcbd4d9b6ad28e5b8d23
GrifterInk <jamie.stump@gmail.com>
2020-10-17 01:17:42 -0700

    PlanRoad minimally working - 1 spawnPoint to 1 energy source

--- src/actions/action.PlanRoad.ts
+++ src/actions/action.PlanRoad.ts
@@ -1,37 +1,49 @@
 import { PathStrokes } from "constants/enum.PathStrokes";
-
+import { CreepMemory } from "interfaces/interface.CreepMemory";
 
 export class actionPlanRoad {
     constructor() {
-
-
     }
 
     Execute(creep: Creep) {
         var startingPoints = this.goToSpawnStart(creep);
-        let startingPointSet: boolean = false;
 
-        if (startingPoints && startingPoints.length > 0) {
+        var currentAction = (creep.memory as CreepMemory).CurrentAction;
+
+        if ((currentAction == "Return to SpawnPoint" || !currentAction) && startingPoints && startingPoints.length > 0) {
+            //console.log("Planner moving back to Spawn Point: " + startingPoints);
             creep.moveTo(startingPoints[0], { visualizePathStyle: { stroke: PathStrokes.SupplyEnergy } });
-            startingPointSet = true;
+            creep.pos.createConstructionSite(STRUCTURE_ROAD);
+
+            var spawnX = startingPoints[0].pos.x;
+            var spawnY = startingPoints[0].pos.y;
+            var creepX = creep.pos.x;
+            var creepY = creep.pos.y;
+
+            //console.log("Spawn Pos: " + spawnX + " / " + spawnY + " | Creep Pos: " + creepX + " / " + creepY);
+            if ((spawnX - creepX >= -1 && spawnX - creepX <= 1) && (spawnY - creepY >= -1 && spawnY - creepY <= 1)) {
+                (creep.memory as CreepMemory).CurrentAction = "Travel to Energy Source"
+            }
         }
         else {
             //Find a different origin point.
         }
 
-        if (startingPointSet) {
+        if (currentAction == "Travel to Energy Source") {
             let energySource: Source | undefined = this.findPathToEnergySource(creep);
 
             if (energySource) {
-                var path = creep.pos.findPathTo(energySource.pos.x, energySource.pos.y);
+                creep.moveTo(energySource, { visualizePathStyle: { stroke: PathStrokes.SupplyEnergy } });
+                creep.pos.createConstructionSite(STRUCTURE_ROAD);
 
-                if (path && path.length > 0) {
-                    path.forEach(pos => {
-                        let roomTerrain: RoomTerrain = creep.room.getTerrain();
+                var sourceX = energySource.pos.x;
+                var sourceY = energySource.pos.y;
+                var creepX = creep.pos.x;
+                var creepY = creep.pos.y;
 
-                        var posTerrain = roomTerrain.get(pos.x, pos.y);
-                        console.log(posTerrain + " = posTerrain");
-                    });
+                //console.log("Source Pos: " + sourceX + " / " + sourceY + " | Creep Pos: " + creepX + " / " + creepY);
+                if ((sourceX - creepX >= -1 && sourceX - creepX <= 1) && (sourceY - creepY >= -1 && sourceY - creepY <= 1)) {
+                    (creep.memory as CreepMemory).CurrentAction = "Return to SpawnPoint"
                 }
             }
         }
